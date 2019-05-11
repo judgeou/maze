1629a2089f27e3c017af1e127a00e92219df9bb0
ouzian <judgeou@vip.qq.com>
2019-05-11 18:39:52 +0800

    最简单的二维数组迷宫

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div-matirx></div-matirx>
+  </div>
+</template>
+
+<script>
+import DivMatirx from './components/DivMatrix.vue'
+
+export default {
+  components: {
+    DivMatirx
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/DivMatrix.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Hello div
+  </div>
+</template>
+
+<script>
+import { solveMaze } from '../lib/matrix'
+
+const m = [
+  [1, 1, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 1, 0],
+  [0, 0, 0, 1, 1]
+]
+
+export default {
+  created () {
+    console.log(
+      solveMaze(m, [0, 0], [3, 4])
+    )
+  }
+}
+</script>
+
+<style>
+
+</style>
