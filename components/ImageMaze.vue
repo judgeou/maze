71a1a2d984d735c5ee82ac4d6a29874574c78236
ouzian <judgeou@vip.qq.com>
2019-05-18 01:12:34 +0800

    引入jimp，解决取像素点慢的问题

--- components/ImageMaze.vue
+++ components/ImageMaze.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img ref="image" src="../assets/maze.gif" alt="">
+    <img ref="image" src="../assets/maze2.png" alt="">
     <canvas ref="canvas" @click="onCanvasClick"></canvas>
     <button @click="goSolve">solve</button>
     <button @click="clearPaths">clear</button>
@@ -9,63 +9,69 @@
 
 <script>
 import { solveMaze } from '../lib/matrix'
+import Jimp from 'jimp'
 
 export default {
   data () {
     return {
       mapArr: [],
-      clickPoints: []
+      clickPoints: [],
+      width: 0,
+      height: 0
     }
   },
   methods: {
-    goSolve (startXY, endXY) {
-      const { paths } = solveMaze(this.mapArr, startXY, endXY)
+    goSolve (startXY, endXY, width, height) {
+      const { paths } = solveMaze(this.mapArr, startXY, endXY, width, height)
       const { canvas } = this.$refs
       const context = canvas.getContext('2d')
       context.fillStyle = "#FF0000"
       for (let path of paths) {
-        context.fillRect(path[0], path[1], 1, 1)
+        context.fillRect(path[0], path[1], 2, 2)
       }
     },
     onCanvasClick (e) {
-      const { clickPoints } = this
+      const { clickPoints, width, height } = this
       clickPoints.push([e.offsetX, e.offsetY])
       const { length } = clickPoints
       if (length >= 2) {
-        this.goSolve(clickPoints[length - 2], clickPoints[length - 1])
+        this.goSolve(clickPoints[length - 2], clickPoints[length - 1], width, height)
       }
     },
-    clearPaths () {
+    async clearPaths () {
       const { image, canvas } = this.$refs
       const { width, height } = image
       canvas.width = width
       canvas.height = height
       const context = canvas.getContext('2d')
       context.drawImage(image, 0, 0, width, height);
+
+      const mapArr = []
+      const imageData = await Jimp.read(image.src)
+      imageData.scan(0, 0, imageData.bitmap.width, imageData.bitmap.height, (x, y, idx) => {
+        const red = imageData.bitmap.data[idx + 0]
+        const green = imageData.bitmap.data[idx + 1]
+        const blue = imageData.bitmap.data[idx + 2]
+        if (red === 255 && green === 255 && blue === 255) {
+          mapArr.push(1)
+        } else {
+          mapArr.push(0)
+        }
+      })
+
+      this.mapArr = mapArr
+
+      this.width = imageData.bitmap.width
+      this.height = imageData.bitmap.height
+
       this.clickPoints = []
     }
   },
+  created () {
+    window.vm = this
+  },
   mounted () {
-    const { image, canvas } = this.$refs
-    const { width, height } = image
-    canvas.width = width
-    canvas.height = height
-    const context = canvas.getContext('2d')
-    context.drawImage(image, 0, 0, width, height);
-    
-    const mapArr = []
-    for (let x = 0; x < width; x++) {
-      const row = mapArr[x] = []
-      for (let y = 0; y < height; y++) {
-        const uint = context.getImageData(x, y, 1, 1).data
-        if (uint[0] === 255 && uint[1] === 255) {
-          row[y] = 1
-        } else {
-          row[y] = 0
-        }
-      }
-    }
-    this.mapArr = mapArr
+
   }
 }
 </script>
