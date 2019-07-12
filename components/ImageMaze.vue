cd7aacce3b043bec7b9a6e295c3a16442ac56e76
ouzian <judgeou@vip.qq.com>
2019-07-12 18:20:16 +0800

    颜色判断符合视觉

--- components/ImageMaze.vue
+++ components/ImageMaze.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <input type="text" v-model="imgUrl">
-    <img ref="image" :src="imgUrl" alt="">
+    <img ref="image" src="../assets/watuji.jpg" alt="">
     <canvas ref="canvas" @click="onCanvasClick"></canvas>
-    <button @click="goSolve">solve</button>
     <button @click="clearPaths">clear</button>
   </div>
 </template>
@@ -11,6 +10,11 @@
 <script>
 import { solveMaze } from '../lib/matrix'
 import Jimp from 'jimp'
+import { rgb2lab, deltaE } from '../lib/color'
+
+function colorDiff (c1, c2) {
+  return deltaE(rgb2lab([ c1.r, c1.g, c1.b ]), rgb2lab([ c2.r, c2.g, c2.b ]))
+}
 
 export default {
   data () {
@@ -32,11 +36,41 @@ export default {
         context.fillRect(path[0], path[1], 1, 1)
       }
     },
-    onCanvasClick (e) {
+    genMapArr (imageData, mapArr, walkColor, stopColor) {
+      console.log(walkColor, stopColor)
+      console.log( colorDiff(walkColor, stopColor) )
+      let i = 0
+      imageData.scan(0, 0, imageData.bitmap.width, imageData.bitmap.height, (x, y, idx) => {
+        const red = imageData.bitmap.data[idx + 0]
+        const green = imageData.bitmap.data[idx + 1]
+        const blue = imageData.bitmap.data[idx + 2]
+        const colorDistance = colorDiff(walkColor, { r: red, g: green, b: blue })
+
+        if (colorDistance < 8) {
+          mapArr[i] = 1
+        } else {
+          mapArr[i] = 0
+        }
+        i++
+      })
+    },
+    async onCanvasClick (e) {
       const { clickPoints, width, height } = this
+      const { image } = this.$refs
       clickPoints.push([e.offsetX, e.offsetY])
       const { length } = clickPoints
       if (length >= 2) {
+        const mapArr = new Array(width * height)
+        const imageData = await Jimp.read(image.src)
+
+        const startPoint = clickPoints[length - 2]
+        const endPoint = clickPoints[length - 1]
+
+        const startColor = Jimp.intToRGBA(imageData.getPixelColor(startPoint[0], startPoint[1]))
+        const endColor = Jimp.intToRGBA(imageData.getPixelColor(endPoint[0], endPoint[1]))
+
+        this.genMapArr(imageData, mapArr, startColor, endColor)
+        this.mapArr = mapArr
         this.goSolve(clickPoints[length - 2], clickPoints[length - 1], width, height)
       }
     },
@@ -46,27 +80,11 @@ export default {
       canvas.width = width
       canvas.height = height
 
-      const mapArr = new Array(width * height)
-      const imageData = await Jimp.read(image.src)
-      let i = 0
-      imageData.scan(0, 0, imageData.bitmap.width, imageData.bitmap.height, (x, y, idx) => {
-        const red = imageData.bitmap.data[idx + 0]
-        const green = imageData.bitmap.data[idx + 1]
-        const blue = imageData.bitmap.data[idx + 2]
-        if (red === 255 && green === 255 && blue === 255) {
-          mapArr[i] = 1
-        } else {
-          mapArr[i] = 0
-        }
-        i++
-      })
-      this.mapArr = mapArr
-
       const context = canvas.getContext('2d')
       context.drawImage(image, 0, 0, width, height);
 
-      this.width = imageData.bitmap.width
-      this.height = imageData.bitmap.height
+      this.width = width
+      this.height = height
 
       this.clickPoints = []
     }
